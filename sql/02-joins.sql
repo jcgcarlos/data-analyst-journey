d7aee7ddd6bb2316515c33b6850c5c88172e8a00
John Christoper Carlos <jcgcarlos95@gmail.com>
2026-03-17 10:38:24 +0800

    added notes and practice code for multi-table, multi-column, and self joins

--- sql/02-joins.sql
+++ sql/02-joins.sql
@@ -55,23 +55,64 @@ WHERE	hs.country IS NULL;
 -- FULL OUTER returns ALL records from BOTH tables, including non-matching records
         
 -- 3. Joining on multiple columns
+SELECT * FROM happiness_scores;
+SELECT * FROM inflation_rates;
 
+SELECT	hs.year, hs.country, ir.inflation_rate
+FROM	happiness_scores as hs
+		INNER JOIN inflation_rates as ir
+        ON hs.year = ir.year AND hs.country = ir.country_name;	 
         
 -- 4. Joining multiple tables
+SELECT * FROM happiness_scores;
+SELECT * FROM country_stats;
+SELECT * FROM inflation_rates;
 
+SELECT 	hs.year, hs.country, hs.happiness_score,
+		cs.continent, cs.population, ir.inflation_rate
+FROM 	happiness_scores as hs
+		LEFT JOIN country_stats as cs
+        ON hs.country = cs.country
+        LEFT JOIN inflation_rates ir
+        ON hs.year = ir.year AND hs.country = ir.country_name;
 
 -- 5. Self joins
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(100),
+    salary INT,
+    manager_id INT
+);
 
+INSERT INTO employees (employee_id, employee_name, salary, manager_id) VALUES
+	(1, 'Ava', 85000, NULL),
+	(2, 'Bob', 72000, 1),
+	(3, 'Cat', 59000, 1),
+	(4, 'Dan', 85000, 2);
+    
+SELECT * FROM employees;
 
 -- Employees with the same salary
-
+SELECT	e1.employee_name, e1.salary,
+		e2.employee_name, e2.salary
+FROM	employees e1 INNER JOIN employees e2
+		ON e1.salary = e2.salary
+WHERE 	e1.employee_id > e2.employee_id;
 
 -- Employees that have a greater salary
-
+-- JOIN Conditions can be changed
+SELECT	e1.employee_name, e1.salary,
+		e2.employee_name, e2.salary
+FROM	employees e1 INNER JOIN employees e2
+		ON e1.salary > e2.salary 
+ORDER BY e1.employee_id;
 
 -- Employees and their managers
-
-        
+SELECT	e1.employee_id, e1.employee_name, e1.manager_id,
+		e2.employee_name AS manager_name
+FROM	employees e1 LEFT JOIN employees e2
+		ON e1.manager_id = e2.employee_id;
+	
 -- 6. Cross joins
 
     
