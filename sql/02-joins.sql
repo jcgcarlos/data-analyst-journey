6615773636b1bd72c066723b20c270bfdef23236
John Christoper Carlos <jcgcarlos95@gmail.com>
2026-03-19 12:35:23 +0800

    answered self join assignment. notes and practice on inner and cross join and union, union all

--- sql/02-joins.sql
+++ sql/02-joins.sql
@@ -114,27 +114,82 @@ FROM	employees e1 LEFT JOIN employees e2
 		ON e1.manager_id = e2.employee_id;
 	
 -- 6. Cross joins
+CREATE TABLE tops (
+    id INT,
+    item VARCHAR(50)
+);
 
+CREATE TABLE sizes (
+    id INT,
+    size VARCHAR(50)
+);
+
+CREATE TABLE outerwear (
+    id INT,
+    item VARCHAR(50)
+);
+
+INSERT INTO tops (id, item) VALUES
+	(1, 'T-Shirt'),
+	(2, 'Hoodie');
+
+INSERT INTO sizes (id, size) VALUES
+	(101, 'Small'),
+	(102, 'Medium'),
+	(103, 'Large');
+
+INSERT INTO outerwear (id, item) VALUES
+	(2, 'Hoodie'),
+	(3, 'Jacket'),
+	(4, 'Coat');
     
 -- View the tables
+SELECT * FROM tops;
+SELECT * FROM sizes;
+SELECT * FROM outerwear;
 
 -- Cross join the tables
-
+SELECT	*
+FROM tops CROSS JOIN sizes;
 
 -- From the self join assignment:
 -- Which products are within 25 cents of each other in terms of unit price?
-
+SELECT	p1.product_name, p1.unit_price,
+		p2.product_name, p2.unit_price,
+        p1.unit_price - p2.unit_price AS price_diff
+FROM	products p1 INNER JOIN products p2
+		ON p1.product_id <> p2.product_id
+WHERE 	ABS(p1.unit_price - p2.unit_price) < 0.25
+		AND p1.product_name < p2.product_name
+ORDER BY price_diff DESC;
         
 -- Rewritten with a CROSS JOIN
-
+SELECT	p1.product_name, p1.unit_price,
+		p2.product_name, p2.unit_price,
+        p1.unit_price - p2.unit_price AS price_diff
+FROM	products p1 CROSS JOIN products p2
+WHERE 	ABS(p1.unit_price - p2.unit_price) < 0.25
+		AND p1.product_name < p2.product_name
+ORDER BY price_diff DESC;
 
 -- 7. Union vs union all
-
+SELECT * FROM tops;
+SELECT * FROM outerwear;
 
 -- Union
-
+SELECT * FROM tops
+UNION
+SELECT * FROM outerwear;
 
 -- Union all
-
+SELECT * FROM tops
+UNION ALL
+SELECT * FROM outerwear;
 
 -- Union with different column names
+SELECT * FROM happiness_scores;
+SELECT * FROM happiness_scores_current;
+
+SELECT year, country, happiness_score FROM happiness_scores
+UNION ALL
+SELECT 2024, country, ladder_score FROM happiness_scores_current;
